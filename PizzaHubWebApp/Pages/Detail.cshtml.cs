e0b5b706097b3a501608c13218e11357e8ad625a
ChisKien <chiskien03214@gmail.com>
2022-03-06 21:05:33 +0700

    Add DetailDao

---
+++ PizzaHubWebApp/Pages/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaHubWebApp.Pages
+{
+    public class Detail : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
