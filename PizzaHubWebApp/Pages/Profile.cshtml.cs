e4d54e3e1cb290e5082caceb53223636d614d9b5
ChisKien <chiskien03214@gmail.com>
2022-03-22 22:23:39 +0700

    Add Profile Page

---
+++ PizzaHubWebApp/Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaHubWebApp.Pages
+{
+    public class Profile : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
