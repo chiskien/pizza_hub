1eca70e87a34d81594b938d2d3f27e165fbbe41b
ChisKien <chiskien03214@gmail.com>
2022-03-22 23:42:31 +0700

    Create DashBoard.cshtml

--- PizzaHubWebApp/Models/PizzaHubContext.cs
+++ PizzaHubWebApp/Models/PizzaHubContext.cs
@@ -37,11 +37,7 @@ namespace PizzaHubWebApp.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=CHISKIEN\\SQLEXPRESS; database=PizzaHub;uid=sa;pwd=sa;");
-            }
+            if (!optionsBuilder.IsConfigured) optionsBuilder.UseSqlServer("");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
