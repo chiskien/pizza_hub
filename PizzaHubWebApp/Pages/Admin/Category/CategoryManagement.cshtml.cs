0785717e0b5a3a5d6af91763df5bef5153b04bf4
ChisKien <chiskien03214@gmail.com>
2022-03-23 16:37:45 +0700

    CategoryManagement.cshtml admin dashboard

---
+++ PizzaHubWebApp/Pages/Admin/Category/CategoryManagement.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaHubWebApp.DAO;
+using PizzaHubWebApp.Models;
+
+namespace PizzaHubWebApp.Pages.Admin.Category
+{
+    public class CategoryManagement : PageModel
+    {
+        private readonly CategoryDao _categoryDao;
+        public IEnumerable<Models.Category> Categories { get; set; }
+
+        public CategoryManagement(PizzaHubContext context)
+        {
+            _categoryDao = new CategoryDao(context);
+        }
+
+        public void OnGet()
+        {
+            Categories = _categoryDao.GetCategories();
+        }
+    }
+}
