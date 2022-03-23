ae1e94eee506f9a955b3d62e2e7b856af0c8032a
ChisKien <chiskien03214@gmail.com>
2022-03-23 16:43:11 +0700

    OrderDao.cs

--- PizzaHubWebApp/DAO/OrderDao.cs
+++ PizzaHubWebApp/DAO/OrderDao.cs
@@ -1,7 +1,27 @@
-﻿namespace PizzaHubWebApp.DAO
+﻿using System.Collections.Generic;
+using System.Linq;
+using PizzaHubWebApp.Models;
+
+namespace PizzaHubWebApp.DAO
 {
     public class OrderDao
     {
-        
+        private readonly PizzaHubContext _context;
+
+        public OrderDao(PizzaHubContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _context.Orders.ToList();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            var order = _context.Orders.Single(o => o.OrderId == id);
+            return order;
+        }
     }
 }
