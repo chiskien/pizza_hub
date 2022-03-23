f99fc1a9f36961ada9b6edad3ed79eccb940492b
ChisKien <chiskien03214@gmail.com>
2022-03-23 16:59:41 +0700

    Implement method GetAllMembers in MemberDao.cs

--- PizzaHubWebApp/DAO/MemberDao.cs
+++ PizzaHubWebApp/DAO/MemberDao.cs
@@ -1,4 +1,5 @@
-﻿using PizzaHubWebApp.Models;
+﻿using System.Collections.Generic;
+using PizzaHubWebApp.Models;
 using System.Linq;
 
 namespace PizzaHubWebApp.DAO
@@ -6,15 +7,22 @@ namespace PizzaHubWebApp.DAO
     public class MemberDao
     {
         private readonly PizzaHubContext _pizzaHubContext;
+
         public MemberDao(PizzaHubContext pizzaHubContext)
         {
             _pizzaHubContext = pizzaHubContext;
         }
+
         public Member GetMemberLogin(string email, string pass)
         {
             Member member = null;
             member = _pizzaHubContext.Members.FirstOrDefault(m => m.Email == email && m.Password == pass);
             return member;
+        }
+
+        public IEnumerable<Member> GetAllMembers()
+        {
+            return _pizzaHubContext.Members.ToList();
         }
     }
 }
