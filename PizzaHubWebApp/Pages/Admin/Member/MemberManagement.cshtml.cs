6edab94fed1812bad08110af0badf4346010b76c
ChisKien <chiskien03214@gmail.com>
2022-03-23 16:59:09 +0700

    MemberDashBoard

--- PizzaHubWebApp/Pages/Admin/Member/MemberManagement.cshtml.cs
+++ PizzaHubWebApp/Pages/Admin/Member/MemberManagement.cshtml.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaHubWebApp.DAO;
+using PizzaHubWebApp.Models;
 
 namespace PizzaHubWebApp.Pages.Admin.Member
 {
     public class MemberManagement : PageModel
     {
+        private readonly MemberDao _memberDao;
+
+        public MemberManagement(PizzaHubContext context)
+        {
+            _memberDao = new MemberDao(context);
+        }
+
         public void OnGet()
         {
-            
         }
+
+        public IEnumerable<Models.Member> Members { get; set; }
     }
 }
