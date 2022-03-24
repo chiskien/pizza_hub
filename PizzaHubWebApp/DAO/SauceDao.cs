6abfdcd0247e6cf82f291288b8b26ec933dd7f16
ChisKien <chiskien03214@gmail.com>
2022-03-24 11:52:00 +0700

    GetAllSauce in SauceDao.cs

---
+++ PizzaHubWebApp/DAO/SauceDao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PizzaHubWebApp.Models;
+
+namespace PizzaHubWebApp.DAO
+{
+    public class SauceDao
+    {
+        private readonly PizzaHubContext _context;
+
+        public SauceDao(PizzaHubContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Sauce> GetAllSauces()
+        {
+            return _context.Sauces.ToList();
+        }
+    }
+}
