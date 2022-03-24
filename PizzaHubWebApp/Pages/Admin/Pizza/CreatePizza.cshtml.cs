d8e1bd69d48fc82084e4cca6b42a878d3182af25
ChisKien <chiskien03214@gmail.com>
2022-03-24 10:12:23 +0700

    choose toppings demo

--- PizzaHubWebApp/Pages/Admin/Pizza/CreatePizza.cshtml.cs
+++ PizzaHubWebApp/Pages/Admin/Pizza/CreatePizza.cshtml.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzaHubWebApp.Pages.Admin.Pizza
 {
     public class CreatePizza : PageModel
     {
+        [BindProperty] public Models.Pizza PizzaModel { get; set; }
+
         public void OnGet()
         {
-            
         }
     }
 }
