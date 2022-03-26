47ee5736022f87e9b340a427fbee9fbee2b707a4
ChisKien <chiskien03214@gmail.com>
2022-03-26 18:53:14 +0700

    Sweet Home "Alabama" Page

--- PizzaHubWebApp/Pages/Login.cshtml.cs
+++ PizzaHubWebApp/Pages/Login.cshtml.cs
@@ -11,27 +11,27 @@ namespace PizzaHubWebApp.Pages
     public class Login : PageModel
     {
         private readonly MemberDao _memberDao;
+
         public Login(PizzaHubContext pizzaHubContext)
         {
             _memberDao = new MemberDao(pizzaHubContext);
         }
+
         public void OnGet()
         {
         }
+
         public void OnPostLogin(string email, string pass)
         {
-            Member member = _memberDao.GetMemberLogin(email, pass);
-            if (member != null){
+            var member = _memberDao.GetMemberLogin(email, pass);
+            if (member != null)
+            {
                 //HttpContext.Session.SetInt32("member", member.MemberId);
                 ViewData["LoginMessage"] = "Login success";
                 if (member.Role == true)
-                {
                     Response.Redirect("Admin/DashBoard");
-                }
                 else
-                {
                     Response.Redirect("/Index");
-                }
             }
             else
             {
@@ -39,20 +39,22 @@ namespace PizzaHubWebApp.Pages
                 Response.Redirect("Login");
             }
         }
+
         public void OnPostSignUp(string email, string pass)
         {
-            Member member = _memberDao.GetMemberByEmail(email);
+            var member = _memberDao.GetMemberByEmail(email);
             if (member == null)
             {
-                Member m = new Member();
+                var m = new Member();
                 m.Email = email;
                 m.Password = pass;
                 _memberDao.AddMember(m);
                 HttpContext.Session.SetInt32("member", _memberDao.GetMemberByEmail(email).MemberId);
-                
-                DirectoryInfo d = new DirectoryInfo(System.IO.Path.GetPathRoot(@"..\..\..\")+ "PizzaHubWebApp\\wwwroot\\Assets\\Avatar\\Bust\\");
-                FileInfo[] Files = d.GetFiles("*.svg");
-                Random r = new Random();
+
+                var d = new DirectoryInfo(Path.GetPathRoot(@"..\..\..\") +
+                                          "PizzaHubWebApp\\wwwroot\\Assets\\Avatar\\Bust\\");
+                var Files = d.GetFiles("*.svg");
+                var r = new Random();
                 m.Avatar = Files[r.Next(0, Files.Length)].Name;
                 ViewData["LoginMessage"] = "Sign Up success";
                 Response.Redirect("Index");
