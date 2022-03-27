8e594e8e5563596e40af761676b85943fb0998b7
ChisKien <chiskien03214@gmail.com>
2022-03-27 20:49:28 +0700

    DropDownList status for OrderManagement.cshtml

--- PizzaHubWebApp/Pages/Admin/Orders/OrderManagement.cshtml.cs
+++ PizzaHubWebApp/Pages/Admin/Orders/OrderManagement.cshtml.cs
@@ -8,17 +8,26 @@ namespace PizzaHubWebApp.Pages.Admin.Orders
     public class OrderManagement : PageModel
     {
         private readonly OrderDao _orderDao;
+        private readonly StatusDao _statusDao;
 
         public OrderManagement(PizzaHubContext context)
         {
+            _statusDao = new StatusDao(context);
             _orderDao = new OrderDao(context);
         }
 
         public IEnumerable<Order> Orders { get; set; }
+        public IEnumerable<Status> Status { get; set; }
+        public int StatusId { get; set; }
 
         public void OnGet()
         {
             Orders = _orderDao.GetAllOrders();
+            Status = _statusDao.GetAllStatus();
+        }
+
+        public void OnPost()
+        {
         }
     }
 }
