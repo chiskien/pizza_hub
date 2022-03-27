76225aff6992e7149136e57c24b6ba37264ac3d2
ChisKien <chiskien03214@gmail.com>
2022-03-27 21:59:28 +0700

    OrderDetailDao.cs

--- PizzaHubWebApp/DAO/OrderDetailDao.cs
+++ PizzaHubWebApp/DAO/OrderDetailDao.cs
@@ -1,7 +1,23 @@
-﻿namespace PizzaHubWebApp.DAO
+﻿using System.Collections.Generic;
+using System.Linq;
+using PizzaHubWebApp.Models;
+
+namespace PizzaHubWebApp.DAO
 {
     public class OrderDetailDao
     {
-        
+        private readonly PizzaHubContext _pizzaHubContext;
+
+        public OrderDetailDao(PizzaHubContext pizzaHubContext)
+        {
+            _pizzaHubContext = pizzaHubContext;
+        }
+
+        public OrdersDetail GetOrderDetailByOrderId(int orderId)
+        {
+            var orderDetail = _pizzaHubContext.OrdersDetails
+                .Single(o => o.OrderId == orderId);
+            return orderDetail;
+        }
     }
 }
--- PizzaHubWebApp/Pages/Admin/Orders/OrderDetail.cshtml.cs
+++ PizzaHubWebApp/Pages/Admin/Orders/OrderDetail.cshtml.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaHubWebApp.DAO;
+using PizzaHubWebApp.Models;
 
 namespace PizzaHubWebApp.Pages.Admin.Orders
 {
     public class OrderDetail : PageModel
     {
-        public void OnGet()
+        private readonly OrderDao _orderDao;
+        private readonly OrderDetailDao _orderDetailDao;
+        private readonly PizzaDao _pizzaDao;
+
+        public OrderDetail(PizzaHubContext context)
         {
-            
+            _orderDetailDao = new OrderDetailDao(context);
+            _pizzaDao = new PizzaDao(context);
+            _orderDao = new OrderDao(context);
+        }
+
+        public OrdersDetail OrdersDetail { get; set; }
+
+        public void OnGet(int id)
+        {
+            OrdersDetail = _orderDetailDao.GetOrderDetailByOrderId(id);
+            OrdersDetail.Pizza = _pizzaDao.GetPizzaById(OrdersDetail.PizzaId);
         }
     }
 }
