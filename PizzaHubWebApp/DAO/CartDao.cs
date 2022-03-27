c330e103321a8055f109de535c9c9754b3446469
ChisKien <chiskien03214@gmail.com>
2022-03-27 19:14:06 +0700

    Add to Cart in CartDao.cs

--- PizzaHubWebApp/DAO/CartDao.cs
+++ PizzaHubWebApp/DAO/CartDao.cs
@@ -22,8 +22,27 @@ namespace PizzaHubWebApp.DAO
             return carts;
         }
 
-        public void AddToCart()
+        public void AddToCart(int pizzaId, int memeberId, int amount)
         {
+            //find if pizza has already in cart
+            var carts = GetCartsByMemberId(memeberId);
+            foreach (var cart in carts)
+                if (cart.PizzaId == pizzaId)
+                {
+                    var existedCart = _context.Carts
+                        .Single(c => c.PizzaId == pizzaId && c.MemberId == memeberId);
+                    existedCart.Amount += amount;
+                    _context.SaveChanges();
+                }
+
+            var newCart = new Cart
+            {
+                MemberId = memeberId,
+                PizzaId = pizzaId,
+                Amount = amount
+            };
+            _context.Carts.Add(newCart);
+            _context.SaveChanges();
         }
     }
 }
